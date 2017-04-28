f90aea69d06dd17cebefd9d469fdcf0902974833
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2017-04-28 17:11:11 +0100

    fixed bad english

--- lang/en/plugin_cs_sms.lang.php
+++ lang/en/plugin_cs_sms.lang.php
@@ -43,4 +43,4 @@ $string['gradebook_md5'] = 'Write md5 of file into file name of gradebook export
 $string['lockfile_lifespan'] = 'Lifespan of process lock files (in hours)';
 $string['active_modules_only'] = 'Only sync enrolments to modules active in Rogo. If enabeld the enrolment feed processing time will be reduced.';
 $string['enable_delete_modules'] = 'Enabled/Disable deletion of modules during module sync.';
-$string['target_module_enrolments'] = 'Enable targeted enrolment sync. At teh cost of addtional API calls to SMS.';
+$string['target_module_enrolments'] = 'Enable targeted enrolment sync. At the cost of additional API calls to the SMS.';
